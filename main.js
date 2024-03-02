9567a13689a54ceb72d6ff6b78e476ff8175a0fd
ryi0 <wpaquette455@gmail.com>
2024-03-02 18:11:13 -0500

    0.1.14  Added conditional jquery operations based on screen size.
     Fixed?  Responsive design for small screen users

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 
 
 $(function () {
-    let navWidth = 5
+    let onPhone = false;
+    if ($(window).width()<600){
+        onPhone = true
+    }
 
-    $('.cocktails').on('mouseover', function() {
-        navWidth=13
-    })
-    $('.cocktails').on('mouseout', function() {
+    if (!onPhone) {
+        let navWidth = 5
+        $('.cocktails').on('mouseover', function () {
+            navWidth = 13
+        })
+        $('.cocktails').on('mouseout', function () {
 
-    })
-    $('.navbar').on('mouseover', function() {
-        navWidth+=7
-        $('.navbar').css('width', `${navWidth}rem`)
-    });
-    $('.navbar').on('mouseout', function() {
-        navWidth=5
-        $('.navbar').css('width', `${navWidth}rem`)
-    });
-
+        })
+        $('.navbar').on('mouseover', function () {
+            navWidth += 7
+            $('.navbar').css('width', `${navWidth}rem`)
+        });
+        $('.navbar').on('mouseout', function () {
+            navWidth = 5
+            $('.navbar').css('width', `${navWidth}rem`)
+        });
+    }
 
     // Initially show home page
     $('.page').removeClass('active');
