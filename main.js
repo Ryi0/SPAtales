77a97ff64de4cdc0005fe0e7de20f8f85b0a3b13
ryi0 <wpaquette455@gmail.com>
2024-03-03 16:10:56 -0500

    0.1.26  Done : Display cocktails list on cocktails page.  Issue : The displayed objects are not formatted.  Lookout? : {Line 99 turns the drink object into an array of values}, {function appendDrinkToGrid could probably separate content to be added by categories with h1, img p tags before adding to grid.}  TODO : format the display of cocktails

--- main.js
+++ main.js
@@ -10,18 +10,20 @@ $(function () {
         $('.cocktails').on('mouseover', function () {
             navWidth = 13
         })
+
         $('.cocktails').on('mouseout', function () {
-
         })
-        $('.navbar').on('mouseover', function () {
 
+        $('.navbar').on('mouseover', function () {
             navWidth += 7
             $('.navbar').css('width', `${navWidth}rem`)
         });
+
         $('.navbar').on('mouseout', function () {
             navWidth = 5
             $('.navbar').css('width', `${navWidth}rem`)
         });
+
         $('.navbar-nav li').on('click', (event)=>{
             if (navOpen) {
                 event.preventDefault();
@@ -48,6 +50,25 @@ $(function () {
 
     })
 });
+/**
+ *
+ * This function displays the cocktails page no matter what page is
+ * @param category
+ */
+function fetchDrinks(category) {
+    console.log(category)
+    $('.page').removeClass('active');
+    $('#cocktails').addClass('active');
+    //finish with :
+
+    closeNav();
+}
+function appendDrinkToGrid(drink){
+    let $drink = $("<div>", {id:"drinkTile", "class":"cocktailsDataDrink"});
+    $drink.text( drink);
+
+    $("#CD").append($drink);
+}
 
 function appendCategoryToFlexbox(displayText) {
     let $Category = $("<div>",{id:"item","class":"categoriesButton"});
@@ -68,26 +89,38 @@ function appendCategoryToFlexbox(displayText) {
     $("#CPC").append($Category2);
 }
 
-/**
- *
- * This function displays the cocktails page no matter what page is
- * @param category
- */
-function fetchDrinks(category) {
-    console.log(category)
-    $('.page').removeClass('active');
-    $('#cocktails').addClass('active');
-    //finish with :
-
-    closeNav();
+let Drinks = [];
+async function DrinksFetcher(){
+    Drinks = [];
+    try {
+        const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail");
+        const data = await response.json()
+        for (const drink of data["drinks"]) {
+            Drinks.push(Object.values(drink))
+            console.log(Object.values(drink))
+        }
+    }catch (e) {
+        console.error("DrinkFetcherBroke : ",e)
+    }
 }
+
+async function DrinkCaller(){
+    await DrinksFetcher();
+    console.log(Drinks);
+    for (const drink of Drinks) {
+        console.log(drink);
+        appendDrinkToGrid(drink)
+    }
+}
+DrinkCaller().catch(e3=>console.error(e3))
+
 let Categories = []
 
 /**
  * this fetches the categories from the db and populates the Categories array
  * @returns {Promise<void>} Drink categories
  */
-async function fetcher(){
+async function CategoriesFetcher(){
         try {
             const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list");
             const data = await response.json()
@@ -104,8 +137,8 @@ async function fetcher(){
  * this permits calling and therefore populating of categories array outside of an async class
  * @returns {Promise<void>}
  */
-async function caller(){
-    await fetcher();
+async function CategoriesCaller(){
+    await CategoriesFetcher();
     console.log(Categories);
     for (const category of Categories) {
         console.log(category)
@@ -113,7 +146,10 @@ async function caller(){
     }
 }
 //Works without catch() but i dont like underlines
-caller().catch(e2=>console.error(e2))
+CategoriesCaller().catch(e2=>console.error(e2))
+
+
+
 
 function openNav() {
     setTimeout(()=>{
