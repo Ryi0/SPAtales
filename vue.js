fa4877b50a3ec961794d8313520f11c59131eb3b
CDiyor <bchristianmax@gmail.com>
2024-03-14 01:56:04 -0400

    Fixed an oopsie

--- vue.js
+++ vue.js
@@ -5,6 +5,13 @@ function Start(onPhone) {
     $(".cocktailInfoPage").hide();
     if (!onPhone) {
         let navWidth = 5;
+        $(".cocktails").on("mouseover", function () {
+            navWidth = 13;
+        });
+
+        $(".cocktails").on("mouseout", function () {
+        });
+
         $(".navbar").on("mouseover", function () {
             navWidth += 7;
             $(".navbar").css("width", `${navWidth}rem`);
